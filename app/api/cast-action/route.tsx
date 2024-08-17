baaae493f0b89762fc5e806408631d84c6c75fcc
Lee Knowlton <leeknowlton@gmail.com>
2024-08-18 00:24:36 +0800

    Add cast action route

---
+++ app/api/cast-action/route.tsx
@@ -0,0 +1,23 @@
+// app/api/cast-action/route.ts
+import { NextRequest } from "next/server";
+import { appURL } from "../../utils";
+import { frames } from "../../frames/frames";
+import { castAction, castActionFrame } from "frames.js/core";
+
+export const GET = async (req: NextRequest) => {
+  return castAction({
+    action: {
+      type: "post",
+    },
+    icon: "pulse",
+    name: "Moxie Fan Token Graph",
+    aboutUrl: `${appURL()}`,
+    description:
+      "Check the price changes of Moxie Fan Tokens and buy/sell them.",
+  });
+};
+
+export const POST = frames(async (ctx) => {
+  console.log("Cast Action FID: " + ctx.message?.castId?.fid);
+  return castActionFrame(`${appURL()}?userfid=${ctx.message?.castId?.fid}`);
+});
